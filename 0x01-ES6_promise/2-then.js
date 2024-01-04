09e5351faa03b569fdacfc02129767513609e269
Abodawoud <yousefabodawoud@gmail.com>
2024-01-04 02:06:59 +0200

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => new Error());
-  console.log('Got a response from the API');
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
