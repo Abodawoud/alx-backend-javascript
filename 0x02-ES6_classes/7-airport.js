beade92e66d061b5edda3c1b3ba8ee3bc8656b7c
Abodawoud <yousefabodawoud@gmail.com>
2024-01-05 01:54:20 +0200

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+
+  toString() {
+    return Object.prototype.toString.call(this._code);
+  }
+}
