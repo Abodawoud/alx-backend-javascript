efe6abc7e45c04a5416b9cc8aa3da7463abd16b4
Abodawoud <yousefabodawoud@gmail.com>
2024-01-10 23:47:52 +0200

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string' || !(set instanceof Set)
+  ) {
+    return '';
+  }
+
   const setIterator = set.values();
   const list = [];
-
   for (const element of setIterator) {
-    if (element.startsWith(startString) && startString !== '') {
+    if (element.startsWith(startString) && typeof element === 'string') {
       list.push(element.slice(startString.length));
     }
   }
