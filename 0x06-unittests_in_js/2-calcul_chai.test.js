91cf16508471ede1a59ff16e7826a4676edd9c11
Abodawoud <yousefabodawoud@gmail.com>
2024-03-02 20:11:25 +0200

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,47 +1,80 @@
+#!/usr/bin/node
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai.js');
 
-const expect = require('chai').expect;
-const calculateNumber = require('./1-calcul.js');
+describe('calculateNumber()', function () {
+  describe('Normal function below .5', function () {
+    it(`should return 3 when the a = 1 b = 2`, function () {
+      expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+    });
+    it(`should return 3 when the a = 1.1 b = 2.2`, function () {
+      expect(calculateNumber('SUM', 1.1, 2.2)).to.equal(3);
+    });
+    it(`should return 1 when the a = 1.1 b = 2.2`, function () {
+      expect(calculateNumber('SUBTRACT', 1.1, 2.2)).to.equal(-1);
+    });
+    it(`should return 0.5 when the a = 1.1 b = 2.2`, function () {
+      expect(calculateNumber('DIVIDE', 1.1, 2.2)).to.equal(0.5);
+    });
+  });
 
-//'Normal function below .5'
-expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-expect(calculateNumber('SUM', 1.1, 2.2)).to.equal(3);
-expect(calculateNumber('SUBTRACT', 1.1, 2.2)).to.equal(-1);
-expect(calculateNumber('DIVIDE', 1.1, 2.2)).to.equal(0.5);
+  describe('Normal function above .5', function () {
+    it(`should return 5 when the a = 1.5 b = 2.5`, function () {
+      expect(calculateNumber('SUM', 1.5, 2.5)).to.equal(5);
+    });
+    it(`should return 1 when the a = 1.5 b = 2.5`, function () {
+      expect(calculateNumber('SUBTRACT', 1.5, 2.5)).to.equal(-1);
+    });
+    it(`should return 1.5 when the a = 2.5 b = 1.5`, function () {
+      expect(calculateNumber('DIVIDE', 2.5, 1.5)).to.equal(1.5);
+    });
+  });
 
-// Normal function above .5
-expect(calculateNumber('SUM', 1.5, 2.5)).to.equal(5);
-expect(calculateNumber('SUBTRACT', 1.5, 2.5)).to.equal(-1);
-expect(calculateNumber('DIVIDE', 2.5, 1.5)).to.equal(1.5);
+  describe('Output: ERROR', function () {
+    it(`should return 'Error' when the a = 1.5 b = 0.2`, function () {
+      expect(calculateNumber('DIVIDE', 1.5, 0.2)).to.equal('Error');
+    });
+    it(`should return 0 when the a = 0 b = 1`, function () {
+      expect(calculateNumber('DIVIDE', 0, 1)).to.equal(0);
+    });
+  });
 
-// should return 'Error' when the a = 1.5 b = 0.2
-expect(calculateNumber('DIVIDE', 1.5, 0.2)).to.equal('Error');
+  describe('Negative Values', function () {
+    it(`should return 'Error' when the a = -1.5 b = -0.2`, function () {
+      expect(calculateNumber('DIVIDE', -1.5, -0.2)).to.equal('Error');
+    });
+    it(`should return -3 when the a = -1.5 b = -2.5`, function () {
+      expect(calculateNumber('SUM', -1.5, -2.5)).to.equal(-3);
+    });
+    it(`should return -1 when the a = -1.5 b = -2.5`, function () {
+      expect(calculateNumber('SUBTRACT', -1.5, -2.5)).to.equal(1);
+    });
+    it(`should return -1 when the a = 2.5 b = 1.5`, function () {
+      expect(calculateNumber('SUBTRACT', 2.5, 1.5)).to.equal(1);
+    });
+    it(`should return 0.5 when the a = -1.5 b = -2.5`, function () {
+      expect(calculateNumber('DIVIDE', -1.5, -2.5)).to.equal(0.5);
+    });
+    it(`should return 0.5 when the a = -1.5 b = -2.5`, function () {
+      expect(calculateNumber('SUBTRACT', 1, 1)).to.equal(0);
+    });
+    it(`should return 0.5 when the a = -1.5 b = -2.5`, function () {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
 
-// should return 0 when the a = 0 b = 1
-expect(calculateNumber('DIVIDE', 0, 1)).to.equal(0);
+  describe('Missing Values', function () {
+    it(`should return NaN when the a = 1.1 b = ???`, function () {
+      expect(calculateNumber('DIVIDE', -1.5)).to.be.NaN;
+    });
 
-// should return 'Error' when the a = -1.5 b = -0.2
-expect(calculateNumber('DIVIDE', -1.5, -0.2)).to.equal('Error');
+    it(`should return NaN when the a = 1.1 b = ???`, function () {
+      expect(calculateNumber('SUM', -1.5)).to.be.NaN;
+    });
 
-// should return -3 when the a = -1.5 b = -2.5
-expect(calculateNumber('SUM', -1.5, -2.5)).to.equal(-3);
-
-// should return 1 when the a = -1.5 b = -2.5
-expect(calculateNumber('SUBTRACT', -1.5, -2.5)).to.equal(1);
-
-// should return 1 when the a = 2.5 b = 1.5
-expect(calculateNumber('SUBTRACT', 2.5, 1.5)).to.equal(1);
-
-// should return 0.5 when the a = -1.5 b = -2.5
-expect(calculateNumber('DIVIDE', -1.5, -2.5)).to.equal(0.5);
-
-// should return 0 when the a = 1 b = 1
-expect(calculateNumber('SUBTRACT', 1, 1)).to.equal(0);
-
-// should return -4 when the a = 1.4 b = 4.5
-expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-
-// 'Missing Values'
-expect(calculateNumber('DIVIDE', -1.5)).to.be.NaN;
-expect(calculateNumber('SUM', -1.5)).to.be.NaN;
-
-expect(() => calculateNumber('SU', 1, 2)).to.throw(TypeError);
+    it(`should throw TypeError`, function () {
+      expect(() => calculateNumber('SU', 1, 2)).to.throw(TypeError);
+    });
+  });
+});
