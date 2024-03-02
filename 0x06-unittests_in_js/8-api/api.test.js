dc5ed01099bc93990d462a743fe14bfc6e6ce77f
Abodawoud <yousefabodawoud@gmail.com>
2024-03-03 01:25:24 +0200

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,30 +1,27 @@
 #!/usr/bin/node
-// api.test.js
-
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const request = require('request');
 const app = require('./api');
-
-const { expect } = chai;
-
-chai.use(chaiHttp);
+const { expect } = require('chai');
 
 describe('Index page', () => {
   it('should return status code 200', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((_, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+    request.get('http://localhost:7865', (_, res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 
   it('should return "Welcome to the payment system"', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((_, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+    request.get('http://localhost:7865', (_, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return status code 404 for non-existing route', (done) => {
+    request.get('http://localhost:7865/non-existing-route', (_, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
   });
 });
